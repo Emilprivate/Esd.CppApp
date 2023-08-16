b8f74438f4b5dc382068c314e5cc4451d20fba8e
Emil <emilsprojects@gmail.com>
2023-08-16 21:36:56 +0200

    Update generate_vs_project.bat

--- generate_vs_project.bat
+++ generate_vs_project.bat
@@ -1,4 +1,18 @@
 @echo off
-mkdir build
+
+REM Create a new directory for the build artifacts
+if not exist "build" (
+    mkdir build
+)
+
+REM Navigate to the build directory
 cd build
-cmake -G "Visual Studio 16 2022" -A x64 ..
+
+REM Set the path to the SDL2 cmake config files
+set SDL2_DIR=C:\SDL2\cmake\
+
+REM Run CMake to configure the project
+cmake -G "Visual Studio 17 2022" -A x64 .. --debug-output
+
+REM Print a success message
+echo Build configuration completed
